9360a6f4743151ba30b5e4318c0f3801054c5bec
AnhNguyen <nd.anh@hutech.edu.vn>
2024-03-17 11:19:22 +0700

    update lưu hình

--- WebBanHangLaptop/Controllers/ProductController.cs
+++ WebBanHangLaptop/Controllers/ProductController.cs
@@ -8,82 +8,72 @@ namespace WebBanHangLaptop.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public ProductController(IProductRepository productRepository)
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
-        //mới thay đổi
-        //public IActionResult Add()
-        //{
-        //    var categories = _categoryRepository.GetAllCategories();
-        //    ViewBag.Categories = new SelectList(categories, "Id", "Name");
-        //    return View();
-        //}
-        //[HttpPost]
-        //public IActionResult Add(Product product)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _productRepository.Add(product);
-        //        return RedirectToAction("Index"); 
-        //    }
-        //    return View(product);
-        //}
-        // Các actions khác như Display, Update, Delete
-        // Display a list of products
+
         public async Task<IActionResult> Index()
         {
             var products = await _productRepository.GetAllAsync();
             return View(products);
         }
-        //// Display a single product
-        //public IActionResult Display(int id)
-        //{
-        //    var product = _productRepository.GetById(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(product);
-        //}
-        //// Show the product update form
-        //public IActionResult Update(int id)
-        //{
-        //    var product = _productRepository.GetById(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(product);
-        //}
-        //// Process the product update
-        //[HttpPost]
-        //public IActionResult Update(Product product)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _productRepository.Update(product);
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(product);
-        //}
-        //// Show the product delete confirmation
-        //public IActionResult Delete(int id)
-        //{
-        //    var product = _productRepository.GetById(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(product);
-        //}
-        //// Process the product deletion
-        //[HttpPost, ActionName("Delete")]
-        //public IActionResult DeleteConfirmed(int id)
-        //{
-        //    _productRepository.Delete(id);
-        //    return RedirectToAction("Index");
-        //}
+
+        // Hiển thị form thêm sản phẩm mới
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+
+            return View("Add");
+        }
+
+        // Xử lý thêm sản phẩm mới
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product, IFormFile imageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageUrl != null)
+                {
+                    // Lưu hình ảnh đại diện
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
+
+                await _productRepository.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            // Nếu ModelState không hợp lệ, hiển thị form với dữ liệu đã nhập
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View(product);
+        }
+
+        private async Task<string> SaveImage(IFormFile image)
+        {
+            var savePath = Path.Combine("wwwroot/images", image.FileName); // Thay đổi đường dẫn theo cấu hình của bạn
+            using (var fileStream = new FileStream(savePath, FileMode.Create))
+            {
+                await image.CopyToAsync(fileStream);
+            }
+            return "/images/" + image.FileName; // Trả về đường dẫn tương đối
+        }
+
+        // Hiển thị form cập nhật sản phẩm
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
     }
 }
